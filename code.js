2e83cd3a1b48b7dd4ffd71a7e4e1bd0d8f1a1941
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-11-25 22:30:07 -0700

    Update code.js
    update description

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function twoOptSwap(route, i, k) {
-    const createRoute = route.slice();
+    const createRoute = route.slice();//equal copied a new one
     const willReverse = createRoute.slice(i + 1, k + 1);
     const reverseParts = willReverse.reverse();
     createRoute.splice( i + 1, reverseParts.length, ...reverseParts);
@@ -22,12 +22,11 @@ function tsp_ls(distance_matrix) {
     var cur = currentRoute.slice();
     // maxIterate: maximum number of iterations
     // I set the default value as 100 because we are doing a small number of iterations
-    // If I change it to Infinity, it will have more computational cost. If I change it to maxIterate = 1000, it will compute more times than maxIterate = 200
-    const maxIterate = 200;
+    // If I change it to Infinity, it will never finished. If I change it to maxIterate = 1000, it will compute more times than maxIterate = inputSize*n
+    const maxIterate = len * len;
     for(var j = 0; j < maxIterate; j++) {
         // i, k is random to choose and make sure they are not the same number
-        // When searching for a solution, it's best to start with bigger steps and gradually decrease the size of each step as you get closer to the end. 
-        // This way, we can make faster progress early on and be more careful as you near the final result.
+        // In each iteration, a new pair of random i and k is chosen
         let i = Math.floor(Math.random() * len);
         let k;
         do {
